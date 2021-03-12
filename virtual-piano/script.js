b1a0c477f51d9ad0a6b16ed26eb53fdb36035ca1
Alexander Naiden <naidenav@gmail.com>
2021-03-12 19:45:47 +0300

    feat: add listeners of mouseover and mouseout

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -7,13 +7,14 @@ function playAudio(src) {
     audio.currentTime = 0;
     audio.play();
 }
-piano.addEventListener('click', (event) => {
+
+let monitorOfEvents = {
+    mousedown: false,
+};
+
+piano.addEventListener('mousedown', (event) => {
+    monitorOfEvents.mousedown = true;
     if (event.target.classList.contains('piano-key')) {
-        pianoKeys.forEach((el) => {
-            if (el.classList.contains('piano-key-active')) {
-                el.classList.remove('piano-key-active');
-            }
-        });
         event.target.classList.add('piano-key-active');
         const note = event.target.dataset.note;
         src = `./assets/audio/${note}.mp3`;
@@ -21,10 +22,32 @@ piano.addEventListener('click', (event) => {
     }
 })
 
+piano.addEventListener('mouseover', (event) => {
+    if (monitorOfEvents.mousedown == true) {
+        if (event.target.classList.contains('piano-key')) {
+            event.target.classList.add('piano-key-active');
+            const note = event.target.dataset.note;
+            src = `./assets/audio/${note}.mp3`;
+            playAudio(src);
+        } 
+    }  
+})
+
+window.addEventListener('mouseup', (event) => {
+    monitorOfEvents.mousedown = false;
+    if (event.target.classList.contains('piano-key')) {
+        event.target.classList.remove('piano-key-active');
+    }
+})
+
+piano.addEventListener('mouseout', (event) => {
+    event.target.classList.remove('piano-key-active');
+})
+
 let heldKeys = {};
 
 window.addEventListener('keydown', (event) => {
-    document.querySelector(`div[data-letter=${event.key.toUpperCase()}`).classList.add('piano-key-active');
+    document.querySelector(`div[data-key=${event.code}`).classList.add('piano-key-active');
     if (heldKeys[event.code] == true) {
         return;
     } else if (event.code === 'KeyD') {
@@ -56,6 +79,6 @@ window.addEventListener('keydown', (event) => {
 })
 
 window.addEventListener('keyup', (event) => {
-    document.querySelector(`div[data-letter=${event.key.toUpperCase()}`).classList.remove('piano-key-active');
+    document.querySelector(`div[data-key=${event.code}`).classList.remove('piano-key-active');
     delete heldKeys[event.code];
 })
