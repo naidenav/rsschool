de3de99e6e0e25ddca7a5c9f84186c240651890f
Alexander Naiden <naidenav@gmail.com>
2021-04-19 10:31:13 +0300

    feat: add dark theme

--- online-zoo/pages/map/script.js
+++ online-zoo/pages/map/script.js
@@ -1,5 +1,31 @@
 const navBtn = document.querySelector(".header__nav-btn");
 const menu = document.querySelector(".nav-list");
+const themeSwitcher = document.querySelector(".theme-switcher");
+const themeSlider = document.querySelector(".theme-switcher__slider");
+const body = document.querySelector("body");
+
+if (localStorage.getItem("theme") === "dark") {
+  body.classList.add("dark-mode");
+  body.classList.remove("light-mode");
+  themeSwitcher.classList.add("switcher_chrcked");
+  themeSlider.classList.add("slider_checked");
+}
+
+themeSwitcher.addEventListener("click", () => {
+  if (body.classList.contains("light-mode")) {
+    themeSwitcher.classList.add("switcher_checked");
+    themeSlider.classList.add("slider_checked");
+    body.classList.remove("light-mode");
+    body.classList.add("dark-mode");
+    localStorage.setItem("theme", "dark");
+  } else {
+    themeSwitcher.classList.remove("switcher_checked");
+    themeSlider.classList.remove("slider_checked");
+    body.classList.remove("dark-mode");
+    body.classList.add("light-mode");
+    localStorage.setItem("theme", "light");
+  }
+})
 
 navBtn.addEventListener("click", () => {
   menu.classList.toggle("nav-list_active");
