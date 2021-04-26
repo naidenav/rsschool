17a69dd72e901bc04cc09757c7b9553727eae17c
Alexander Naiden <naidenav@gmail.com>
2021-04-26 22:44:58 +0300

    feat: make watch-online slider workable

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -3,6 +3,9 @@ const menu = document.querySelector(".nav-list");
 const themeSwitcher = document.querySelector(".theme-switcher");
 const themeSlider = document.querySelector(".theme-switcher__slider");
 const body = document.querySelector("body");
+const slidesWatchOnline = document.querySelectorAll(".watch-online__slide");
+const rangeWatchOnline = document.querySelector("[type=range]");
+const carousel = document.querySelector(".watch-online__carousel");
 
 if (localStorage.getItem("theme") === "dark") {
   body.classList.add("dark-mode");
@@ -31,3 +34,48 @@ navBtn.addEventListener("click", () => {
   menu.classList.toggle("nav-list_active");
   navBtn.classList.toggle("nav-btn_active");  
 })
+
+let watchRangeValue = 2;
+let order = 0;
+
+function changeRange() {
+  if (!slidesWatchOnline[rangeWatchOnline.value - 1].classList.contains("slide-active")) {
+    slidesWatchOnline[rangeWatchOnline.value - 1].classList.add("slide-active");
+    slidesWatchOnline[watchRangeValue - 1].classList.remove("slide-active");
+  }
+
+  let shift;
+
+  if (window.screen.width < 640) {
+    shift = 122;
+  } else if (window.screen.width < 1920) {
+    shift = 210;
+  } else {
+    shift = 186;
+  }
+
+  if (watchRangeValue < rangeWatchOnline.value) {
+    document.querySelector(".watch-online__carousel").style.transform = `translateX(-${(rangeWatchOnline.value - 2) * shift}px)`;
+    // if (this.value > 2) {
+    //   slidesWatchOnline[this.value - 3].classList.add("hide-slide");
+    // }
+  } else {
+    document.querySelector(".watch-online__carousel").style.transform = `translateX(${(2 - rangeWatchOnline.value) * shift}px)`;
+    // if (this.value > 1) {
+    //   slidesWatchOnline[this.value - 2].classList.remove("hide-slide");
+    // }
+  }
+
+  watchRangeValue = rangeWatchOnline.value;
+}
+
+rangeWatchOnline.addEventListener("input", changeRange);
+
+carousel.addEventListener("click", function(e) {
+  nodes = Array.prototype.slice.call(slidesWatchOnline);
+  if (e.target.tagName == "IMG") {
+    rangeWatchOnline.value = nodes.indexOf(e.target.parentElement) + 1;
+  }
+  changeRange();
+})
+
